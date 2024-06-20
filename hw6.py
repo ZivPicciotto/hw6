8b2311735d22e2618df750c64c7688f44e5d76d7
Dylan Lewis <dylan.lewis@post.runi.ac.il>
2024-06-21 00:51:15 +0300

    a commit

--- hw6.py
+++ hw6.py
@@ -31,7 +31,6 @@ def lp_distance(X, centroids, p=2):
     all points in RGB space from all centroids
     '''
     distances = []
-    k = len(centroids)
     ###########################################################################
     # TODO: Implement the function.                                           #
     ###########################################################################
@@ -54,20 +53,14 @@ def kmeans(X, k, p ,max_iter=100):
     - The calculated centroids as a numpy array.
     - The final assignment of all RGB points to the closest centroids as a numpy array.
     """
-    classes = []
-    centroids = get_random_centroids(X, k)
     ###########################################################################
     # TODO: Implement the function.                                           #
     ###########################################################################
-    centroids = get_random_centroids(X, k)
-    for _ in range(max_iter):
-        distances = lp_distance(X,centroids, p)
-        classes = np.argmin(distances,axis=0)
-        prev = centroids
-        centroids = np.array([np.mean(X[classes==i,:],axis=0) for i in range(k)])
-
-        if np.all(prev== centroids):
-            break 
+    """ See kmeans_with_itteration_count for implementation details
+        added the function in order to do comparrisons later
+    """
+    # we simply ignore the itterations in case this function is called.
+    centroids, classes, _ = kmeans_with_itteration_count(X,k,p,max_iter)
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -86,14 +79,50 @@ def kmeans_pp(X, k, p ,max_iter=100):
     - The calculated centroids as a numpy array.
     - The final assignment of all RGB points to the closest centroids as a numpy array.
     """
-    classes = None
-    centroids = None
     ###########################################################################
     # TODO: Implement the function.                                           #
     ###########################################################################
-    centroids = kmeans_pp_centroid(X,k, p)
-    print(centroids.shape)
+    """ See kmeans_pp_with_itteration_count for implementation details
+        added the function in order to do comparrisons later
+    """
+    # we simply ignore the itterations in case this function is called.
+    centroids, classes, _ = kmeans_pp_with_itteration_count(X,k,p,max_iter)
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return centroids, classes
+
+def kmeans_pp_centroid(X, k, p) -> np.ndarray:
+    chosen_indexes = [np.random.choice(range(len(X)))]
+    centroids = [X[chosen_indexes[0], :]]
+
+    for _ in range(k - 1):
+        mask = np.ones(len(X), dtype=bool)
+        mask[chosen_indexes] = False
+        remaining = X[mask, :]
+        current_distances = lp_distance(remaining, centroids, p)
+        min_distances = np.min(current_distances, axis=0)
+        squares = min_distances**2
+        probabilities = squares / np.sum(squares)
+
+        new_centroid_index = np.random.choice(np.arange(len(remaining)), p=probabilities)
+        chosen_indexes.append(np.where(mask)[0][new_centroid_index])
+        centroids.append(X[chosen_indexes[-1], :])
+
+    return np.array(centroids)
+
+
+"""
+this function does exactly as out original kmeans_pp implementation
+however we wanted to display the number of itterations in a later section
+so in order to not change the original return value of kmeans_pp we made this
+helper function
+"""
+def kmeans_with_itteration_count(X, k, p ,max_iter=100):
+    centroids = get_random_centroids(X, k)
+    itterations = 0
     for _ in range(max_iter):
+        itterations+=1
         distances = lp_distance(X,centroids, p)
         classes = np.argmin(distances,axis=0)
         prev = centroids
@@ -101,32 +130,26 @@ def kmeans_pp(X, k, p ,max_iter=100):
 
         if np.all(prev== centroids):
             break 
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
-    return centroids, classes
 
-def kmeans_pp_centroid(X,k, p) -> np.ndarray:
-    
-    chosen_indexes = [np.random.choice(range(len(X)))]
-    centroids = [X[chosen_indexes[0],:]]
-    
-    for _ in range(k-1):
-        mask = np.ones(len(X), dtype=bool)
-        mask[chosen_indexes] = False
-        remaining = X[mask, : ]
-        current_distances = lp_distance(remaining, centroids, p)
-        classes = np.argmin(current_distances,axis=0)
-        row_indexes = np.arange(len(current_distances.T))
-        min_distances = current_distances[row_indexes, classes]
-        squares = min_distances**2 
-        probabilities = squares / np.sum(squares)
-        print(probabilities.shape)
-        print(row_indexes.shape)
+    return centroids, classes, itterations
 
-        new_centroid_index = np.random.choice(row_indexes, p=probabilities)
-        
-        chosen_indexes.append(np.where(mask)[0][new_centroid_index])
-        centroids.append(X[chosen_indexes[-1], :])
+"""
+this function does exactly as out original kmeans implementation
+however we wanted to display the number of itterations in a later section
+so in order to not change the original return value of kmeans we made this
+helper function
+"""
+def kmeans_pp_with_itteration_count(X, k, p ,max_iter=100):
+    itterations = 0
+    centroids = kmeans_pp_centroid(X,k, p)
+    for _ in range(max_iter):
+        itterations += 1
+        distances = lp_distance(X,centroids, p)
+        classes = np.argmin(distances,axis=0)
+        prev = centroids
+        centroids = np.array([np.mean(X[classes==i,:],axis=0) for i in range(k)])
 
-    return np.array(centroids)
+        if np.all(prev== centroids):
+            break 
+    return centroids, classes, itterations
+
