b45644bb3cb190bc002fa6a141c8411712559559
ziv.picciotto <ziv.picciotto@post.runi.ac.il>
2024-06-20 18:05:42 +0300

    3rd commit

--- hw6.py
+++ hw6.py
@@ -36,7 +36,7 @@ def lp_distance(X, centroids, p=2):
     # TODO: Implement the function.                                           #
     ###########################################################################
     image_reshaped = X[:, np.newaxis, :]
-    distances = np.linalg.norm(image_reshaped-centroids, ord=p,axis=2).T 
+    distances = np.linalg.norm(image_reshaped-centroids, ord=p,axis=2).T
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -91,8 +91,42 @@ def kmeans_pp(X, k, p ,max_iter=100):
     ###########################################################################
     # TODO: Implement the function.                                           #
     ###########################################################################
-    pass 
+    centroids = kmeans_pp_centroid(X,k, p)
+    print(centroids.shape)
+    for _ in range(max_iter):
+        distances = lp_distance(X,centroids, p)
+        classes = np.argmin(distances,axis=0)
+        prev = centroids
+        centroids = np.array([np.mean(X[classes==i,:],axis=0) for i in range(k)])
+
+        if np.all(prev== centroids):
+            break 
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
     return centroids, classes
+
+def kmeans_pp_centroid(X,k, p) -> np.ndarray:
+    
+    chosen_indexes = [np.random.choice(range(len(X)))]
+    centroids = [X[chosen_indexes[0],:]]
+    
+    for _ in range(k-1):
+        mask = np.ones(len(X), dtype=bool)
+        mask[chosen_indexes] = False
+        remaining = X[mask, : ]
+        current_distances = lp_distance(remaining, centroids, p)
+        classes = np.argmin(current_distances,axis=0)
+        row_indexes = np.arange(len(current_distances.T))
+        min_distances = current_distances[row_indexes, classes]
+        squares = min_distances**2 
+        probabilities = squares / np.sum(squares)
+        print(probabilities.shape)
+        print(row_indexes.shape)
+
+        new_centroid_index = np.random.choice(row_indexes, p=probabilities)
+        
+        chosen_indexes.append(np.where(mask)[0][new_centroid_index])
+        centroids.append(X[chosen_indexes[-1], :])
+
+    return np.array(centroids)
